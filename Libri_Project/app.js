ab871eb8c5ecaac7af42acd7381629fb38889e43
Rahul Vijay <rahulvk@Rahuls-MacBook-Air.local>
2017-07-03 13:00:53 +0530

    Backend and front-end

---
+++ Libri_Project/app.js
@@ -0,0 +1,27 @@
+var  express     = require("express"),
+     mongoose    = require("mongoose"),
+     bodyParser  = require("body-parser");
+      app        = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//SHOW LANDING PAGE ROUTE
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+//SHOW SIGN-UP ROUTE
+app.get("/signup", function(req, res){
+  res.render("signUp");
+});
+
+//SHOW LOGIN ROUTE
+app.get("/login", function(req, res){
+  res.render("Login");
+});
+
+//SERVER START
+app.listen(3000, function(req, res){
+  console.log("Server started on Port 3000");
+});
